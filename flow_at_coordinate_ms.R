40d84cb5bb5aa1fe1fc693c4672b588d17673eb8
Moutushi Debnath Sen <42132430+moutushisen@users.noreply.github.com>
2025-09-26 04:30:51 +0200

    Update flow_at_coordinate_ms.R

--- flow_at_coordinate_ms.R
+++ flow_at_coordinate_ms.R
@@ -9,15 +9,15 @@ library(ncdf4)
 #' @return numeric vector (time series) or matrix (profile × time)
 #'
 flow_at_coordinate <- function(ncdf, x, y, direction, profile = FALSE) {
-  # --- Input checks ---
+  # Input checks
   stopifnot(is.character(ncdf), is.numeric(x), is.numeric(y),
             direction %in% c("x", "y"), is.logical(profile))
   
-  # --- Open file ---
+  # Open file
   nc <- nc_open(ncdf)
   on.exit(nc_close(nc))
   
-  # --- Get cell center coordinates ---
+  # Get cell center coordinates
   xt <- ncvar_get(nc, "xt")[, 1]
   yt <- ncvar_get(nc, "yt")[1, ]
   
@@ -25,7 +25,7 @@ flow_at_coordinate <- function(ncdf, x, y, direction, profile = FALSE) {
   ix <- which.min(abs(xt - x))
   iy <- which.min(abs(yt - y))
   
-  # --- Velocity variable ---
+  # Velocity variable
   vel_name <- ifelse(direction == "y", "vk", "uk")
   vel <- ncvar_get(nc, vel_name)
   
@@ -35,21 +35,21 @@ flow_at_coordinate <- function(ncdf, x, y, direction, profile = FALSE) {
   }
   vel_point <- vel[ix, iy, , ]
   
-  # --- Layer thickness ---
+  # Layer thickness
   hnt <- ncvar_get(nc, "hnt")
   if (length(dim(hnt)) == 3L) {
     dim(hnt) <- c(dim(hnt), 1)
   }
   hnt_point <- hnt[ix, iy, , ]
   
-  # --- Grid spacing (interface width) ---
+  # Grid spacing (interface width)
   if (direction == "y") {
     width <- xt[2] - xt[1]
   } else {
     width <- -(yt[2] - yt[1])  # negative: reversed y-order
   }
   
-  # --- Compute flow ---
+  # Compute flow
   flow_matrix <- vel_point * hnt_point * width
   
   if (!profile) {
@@ -59,7 +59,7 @@ flow_at_coordinate <- function(ncdf, x, y, direction, profile = FALSE) {
     flow <- flow_matrix
   }
   
-  # --- Fix sign for y ---
+  # Fix sign for y
   if (direction == "y") {
     sign_y <- ifelse(all(diff(yt) > 0), 1, -1)
     flow <- sign_y * flow
@@ -74,3 +74,4 @@ flow1 <- flow_at_coordinate("malaren_3d_20190801_multi_t.nc",x = 658350, y = 659
 
 # Depth profile (layers × time)
 flow2 <- flow_at_coordinate("malaren_3d_20190801_multi_t.nc",x = 658350, y = 6596550,direction = "y", profile = TRUE)
+
